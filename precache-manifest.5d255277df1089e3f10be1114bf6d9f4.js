f67d1573c42460647098e6b43e5ef87e2e666f2e
Gray Newfield <gnewfield@Grays-MacBook-Pro.local>
2019-05-02 12:16:28 -0400

    initial commit

---
+++ precache-manifest.5d255277df1089e3f10be1114bf6d9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "512483ed47aad9ebc09e",
+    "url": "/static/css/main.86cee6fe.chunk.css"
+  },
+  {
+    "revision": "512483ed47aad9ebc09e",
+    "url": "/static/js/main.512483ed.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "79898f04398a8b6e37ae",
+    "url": "/static/js/2.79898f04.chunk.js"
+  },
+  {
+    "revision": "697c4816753f0d0d40a1d9c5c12d81b8",
+    "url": "/static/media/logo.697c4816.png"
+  },
+  {
+    "revision": "67e4b2f3084732e94087aae4921f974e",
+    "url": "/index.html"
+  }
+];
